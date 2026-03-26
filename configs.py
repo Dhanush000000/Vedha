8fd49dc839b723dd31b362ec3acb4927bbf93077
Dhanush000000 <100513826+Dhanush000000@users.noreply.github.com>
2026-03-26 18:22:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,10 +8,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "25038048"))
     API_HASH = getenv("API_HASH", "09e892474901472e030fcdd53fb7384a")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7979346594:AAFjYirmIPiMfEfT2WbHKrG53PLX6astM5E")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8795241058:AAE_P6Y-ADpFY8PYC8bHPJ32NdL5rOR_NZI")
     SESSION_STRING = getenv("SESSION_STRING", "")
     SUDO = list(map(int, getenv("SUDO", "5798247275 5612704084").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Loocifar123:Loocifar123@cluster0.jkxxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://kumar167:kumar167@cluster0.mi8qi5l.mongodb.net/?appName=Cluster0")
 
 cfg = Config()
 
